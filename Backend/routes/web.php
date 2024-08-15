dc9d72c17e9073618a7dc67f4a95bbe8e6b678c7
stonefadel02 <stone.afedjou@epitech.eu>
2024-08-15 21:58:53 +0100

    update

---
+++ Backend/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get("login-register", "SocialiteController@loginRegister");
+
+Route::get("redirect/{provider}", "SocialiteController@redirect")->name('socialite.redirect');
+
+Route::get("callback/{provider}", "SocialiteController@callback")->name('socialite.callback');
+
+
+require __DIR__.'/auth.php';
